d270b36cfead2aace00761d755ee7c59643303bf
Matuesz <mj@mj-laptop.(none)>
2010-05-08 09:00:05 +0200

    coAKQ first test

--- test/unit/COAKQTest.cpp
+++ test/unit/COAKQTest.cpp
@@ -84,6 +84,40 @@ BOOST_AUTO_TEST_CASE(coreduction_emptyTriangle) {
 
 }
 
+BOOST_AUTO_TEST_CASE(coreduction_fullTriangle) {
+  typedef SComplex<SComplexDefaultTraits> Complex;
+
+
+  Complex::Dims dims = list_of(0)(1)(0)(1)(0)(1)(2);
+  Complex::KappaMap kappaMap = tuple_list_of(1, 0, -1)(1, 2, 1)(3, 2, -1)(3, 4, 1)(5, 4, -1)(5, 0, 1)
+	 (6, 1, -1)(6, 3, 1)(6, 5, -1); 
+  
+  Complex complex(3, dims, kappaMap, 1);
+
+  COAKQAlgorithm<COAKQStrategy<Complex, Complex> > algorithm(new COAKQStrategy<Complex, Complex>(complex));
+
+  algorithm();
+
+  BOOST_FOREACH(Complex::Iterators::AllCells::iterator::value_type v,
+					 complex.iterators().allCells()) {
+	 BOOST_CHECK_EQUAL(v.getColor(), (Complex::Color)2);
+  }
+
+  BOOST_CHECK(complex.iterators(1).allCells().begin() == complex.iterators(1).allCells().end());  
+
+  Complex& coAKQ = algorithm.getStrategy().getOutputComplex();
+  BOOST_CHECK_EQUAL(coAKQ.size(), 2);
+
+  CRef<ReducibleFreeChainComplexType> RFCComplexCR=
+  	 (ReducibleFreeChainComplexOverZFromSComplexAlgorithm<Complex, ReducibleFreeChainComplexType>(coAKQ))();
+  CRef<HomologySignature> homSignCR=HomAlgFunctors<FreeModuleType>::homSignViaAR_Random(RFCComplexCR);
+
+  std::string betti = homSignCR().bettiVector();
+
+  BOOST_CHECK_EQUAL(betti, "");
+
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
 
