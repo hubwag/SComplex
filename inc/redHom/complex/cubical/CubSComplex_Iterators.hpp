26d1d12fcbecf6c642f4d63341e555ca9ca6afc3
Matuesz <mj@mj-laptop.(none)>
2010-06-06 14:38:57 +0200

    Cubical complex iterators for boost_foreach

--- inc/redHom/complex/cubical/CubSComplex_Iterators.hpp
+++ inc/redHom/complex/cubical/CubSComplex_Iterators.hpp
@@ -16,8 +16,6 @@ template<bool isConst>
 class CubSComplex<DIM>::IteratorsImpl {
   typedef typename boost::mpl::if_c<isConst, const CubSComplex&, CubSComplex&>::type SComplexRef;
 public:
-  typedef typename CubSComplex::template IteratorProvider<CellNumerator, isConst> AllCells;
-  typedef typename CubSComplex::template IteratorProvider<BdNumerator, isConst> BdCells;  
 
   typedef CellProxy<BitCoordPtrCellImpl> CellType;
 
@@ -119,19 +117,63 @@ public:
 	 Dim dim;  
   };
 
+  class AllCellsCoordIterator: public  CoordIterator<AllCellsCoordIterator> {
+	 typedef typename CubSComplex::BCubCellSet::BitCoordIterator BitCoordIterator;
+  public:
+  
+    AllCellsCoordIterator(const CubSComplex& s, int): CoordIterator<AllCellsCoordIterator>(), complex(&s) { //begin iterator
+	 }
+
+    AllCellsCoordIterator(const CubSComplex& s): CoordIterator<AllCellsCoordIterator>(), complex(NULL) { //end constructor
+    }
+  
+  private:	 
+	 friend class boost::iterator_core_access;
+	 friend class CoordIterator<AllCellsCoordIterator>;
+	 
+	 void initCoordIt(BitCoordIterator* allocatedMemory) {
+		if (complex == NULL) {
+		  coordIt = NULL;
+		} else {
+		  coordIt = new (allocatedMemory) BitCoordIterator(complex->bCubCellSet);
+		  findFirst();
+		}
+	 }
+	 
+	 void findFirst() {
+		while (coordIt->wIt < coordIt->getBitmap().end().wIt) {
+		  if (!coordIt->findPoint())
+		    break;
+		  if(coordIt->getBit())
+		    return;
+		  ++(*coordIt);
+		}
+		coordIt = NULL;
+	 }
+  
+	 void increment() {
+		++(*CoordIterator<AllCellsCoordIterator>::getCoordIt());
+		findFirst();
+	 }
+
+	 using CoordIterator<AllCellsCoordIterator>::coordIt;	 
+	 const CubSComplex* complex;
+  };
+
   class CbdCoordIterator: public  CoordIterator<CbdCoordIterator> {
 	 typedef typename CubSComplex::BCubCellSet::BitCoordIterator BitCoordIterator;
   public:
 	 CbdCoordIterator(): CoordIterator<CbdCoordIterator>() {}
 
 	 template<typename ImplT>
-	 CbdCoordIterator(CubSComplex& s, const CellProxy<ImplT>& c): CoordIterator<CbdCoordIterator>(), sourceCoordIt(&c.getBitCoordIt()),
-																							i(-1), dim(s.bCubCellSet.embDim()) { //begin iterator
+	 CbdCoordIterator(CubSComplex& s, const CellProxy<ImplT>& c): CoordIterator<CbdCoordIterator>(), 
+								      sourceCoordIt(&c.getBitCoordIt()),
+								      i(-1), dim(s.bCubCellSet.embDim()) { //begin iterator
 	 }
 
 	 CbdCoordIterator(CubSComplex& s): CoordIterator<CbdCoordIterator>(), sourceCoordIt(NULL),
-												  i(0), dim(0) { //end constructor
-		toEnd();
+					   i(0), dim(0) { //end constructor
+	   toEnd();
 	 }
   
   private:	 
@@ -197,19 +239,105 @@ public:
 	 Dim dim;
   };
 
+  class BdCoordIterator: public  CoordIterator<BdCoordIterator> {
+	 typedef typename CubSComplex::BCubCellSet::BitCoordIterator BitCoordIterator;
+  public:
+	 BdCoordIterator(): CoordIterator<BdCoordIterator>() {}
+
+	 template<typename ImplT>
+	 BdCoordIterator(CubSComplex& s, const CellProxy<ImplT>& c): CoordIterator<BdCoordIterator>(), 
+								      sourceCoordIt(&c.getBitCoordIt()),
+								      i(-1), dim(s.bCubCellSet.embDim()) { //begin iterator
+	 }
+
+	 BdCoordIterator(CubSComplex& s): CoordIterator<BdCoordIterator>(), sourceCoordIt(NULL),
+					   i(0), dim(0) { //end constructor
+	   toEnd();
+	 }
+  
+  private:	 
+	 friend class boost::iterator_core_access;
+	 friend class  CoordIterator<BdCoordIterator>;
+	 
+	 void initCoordIt(BitCoordIterator* allocatedMemory) {
+		if (sourceCoordIt == NULL) {
+		  coordIt = NULL;
+		} else {
+		  coordIt = new (allocatedMemory) BitCoordIterator(*sourceCoordIt);
+		  findBd();
+		}
+	 }
+
+	 void toEnd() {
+		coordIt = NULL;
+	 }
+	 
+	 void findBd() {
+		if (i > -1 && i/2 < dim) {
+		  if (i%2 == 0) {
+			 coordIt->incInDir(i/2);
+		  } else {
+			 coordIt->decInDir(i/2);
+		  }
+		}
+
+		++i;
+		while(i/2 < dim) {
+		  while(i/2 < dim && !coordIt->odd(i/2)) {
+			 i += 2;
+		  }
+		  if (i/2 < dim) {
+			 if (i%2 == 0) {
+				coordIt->decInDir(i/2);
+			 } else {
+				coordIt->incInDir(i/2);
+			 }
+			 if (coordIt->getBit()) {
+				return;
+			 } else {
+				if (i%2 == 0) {
+				  coordIt->incInDir(i/2);
+				} else {
+				  coordIt->decInDir(i/2);
+				}
+				++i;
+			 }
+		  }
+		}
+		toEnd();
+	 }
+  
+	 void increment() {
+		CoordIterator<BdCoordIterator>::getCoordIt();
+		findBd();
+	 }
+
+	 using CoordIterator<BdCoordIterator>::coordIt;
+	 const BitCoordIterator* sourceCoordIt;	 
+	 int i;
+	 Dim dim;
+  };
+
+  typedef typename boost::iterator_range<AllCellsCoordIterator > AllCells;
   typedef typename boost::iterator_range<DimCoordIterator > DimCells;
   typedef typename boost::iterator_range<CbdCoordIterator > CbdCells;
-
+  typedef typename boost::iterator_range<BdCoordIterator > BdCells;
 
   IteratorsImpl(SComplexRef _scomplex): scomplex(_scomplex) {}
   
-  AllCells allCells() const {   return AllCells(CellNumerator(scomplex)); }
+
+  AllCells allCells() {return AllCells(AllCellsCoordIterator(scomplex, 0), AllCellsCoordIterator(scomplex));}
   DimCells dimCells(const Dim& dim) {return DimCells(DimCoordIterator(scomplex, dim), DimCoordIterator(scomplex));}
-  BdCells bdCells(const Cell& cell) const {return BdCells(BdNumerator(scomplex, cell)); }
+
 
   template<typename ImplT>
   CbdCells cbdCells(const CellProxy<ImplT>& cell) const {
 	 return CbdCells(CbdCoordIterator(scomplex, cell), CbdCoordIterator(scomplex));
+  }
+
+  template<typename ImplT>
+  BdCells bdCells(const CellProxy<ImplT>& cell) const {
+	 return BdCells(BdCoordIterator(scomplex, cell), BdCoordIterator(scomplex));
   }
   
 private:
--- test/unit/CubSComplexIteratorsTest.cpp
+++ test/unit/CubSComplexIteratorsTest.cpp
@@ -2,6 +2,7 @@
 #include <boost/bind.hpp>
 #include <boost/lambda/lambda.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/foreach.hpp>
 #include <algorithm>
 
 #include <iostream>
@@ -51,20 +52,86 @@ BOOST_AUTO_TEST_CASE(iterators_AllCells_forEach_test) {
 	 BOOST_CHECK_EQUAL(s->cardinality(), 3456);
 
 	 int cnt = 0;
-	 for (SComplex::Iterators::AllCells::iterator it = s->iterators(1).allCells().begin();
-			it != s->iterators(1).allCells().end(); ++it) {
-		if ((*it).getColor() == 1) {
+	 BOOST_FOREACH(SComplex::Iterators::AllCells::iterator::value_type v, s->iterators(1).allCells()) {
+		if (v.getColor() == 1) {
 		  ++cnt;
 		}
 	 }
 	 BOOST_CHECK_EQUAL(cnt, initSize);
 	 
-	 std::for_each(s->iterators(1).allCells().begin(), s->iterators(1).allCells().end(), boost::bind(&SComplex::Cell::setColor, _1, 2));
-	 BOOST_CHECK_EQUAL(s->cardinality(), 0);
+// 	 SComplex::Iterators::AllCells allCells = s->iterators(1).allCells();
+// 	 std::for_each(allCells.begin(), allCells.end(), boost::bind(&SComplex::Cell::setColor, _1, 2));
+// 	 BOOST_CHECK_EQUAL(s->cardinality(), 0);
 
-	 cnt = 0;
-	 std::for_each(s->iterators(1).allCells().begin(), s->iterators(1).allCells().end(), boost::lambda::var(cnt)++);
+// 	 cnt = 0;
+// 	 std::for_each(s->iterators(1).allCells().begin(), s->iterators(1).allCells().end(), boost::lambda::var(cnt)++);
 	 //	 BOOST_CHECK_EQUAL(cnt, initSize);	 
+}
+
+
+BOOST_AUTO_TEST_CASE(iterators_BdCells_forEach_test) {
+  typedef CubSComplex<3> SComplex;
+  boost::shared_ptr<SComplex> s = createComplex();
+
+  
+  SComplex::Iterators::DimCells::iterator::value_type dim0Cell = *(s->iterators(1).dimCells(0).begin());
+  int dim0CellBdSize = 0;
+
+  BOOST_FOREACH(SComplex::Iterators::BdCells::iterator::value_type v, s->iterators(1).bdCells(dim0Cell)) {
+    dim0CellBdSize++;
+  }
+  BOOST_CHECK_EQUAL(dim0CellBdSize, 0);
+
+  SComplex::Iterators::DimCells::iterator::value_type dim1Cell = *(s->iterators(1).dimCells(1).begin());
+  int dim1CellBdSize = 0;
+
+  BOOST_FOREACH(SComplex::Iterators::BdCells::iterator::value_type v, s->iterators(1).bdCells(dim1Cell)) {
+    dim1CellBdSize++;
+  }
+  BOOST_CHECK_EQUAL(dim1CellBdSize, 2);
+
+
+  SComplex::Iterators::DimCells::iterator::value_type dim2Cell = *(s->iterators(1).dimCells(2).begin());
+  int dim2CellBdSize = 0;
+
+  BOOST_FOREACH(SComplex::Iterators::BdCells::iterator::value_type v, s->iterators(1).bdCells(dim2Cell)) {
+    dim2CellBdSize++;
+  }
+  BOOST_CHECK_EQUAL(dim2CellBdSize, 4);
+
+}
+
+
+BOOST_AUTO_TEST_CASE(iterators_CbdCells_forEach_test) {
+  typedef CubSComplex<3> SComplex;
+  boost::shared_ptr<SComplex> s = createComplex();
+
+  
+  SComplex::Iterators::DimCells::iterator::value_type dim0Cell = *(s->iterators(1).dimCells(0).begin());
+  int dim0CellCbdSize = 0;
+
+  BOOST_FOREACH(SComplex::Iterators::CbdCells::iterator::value_type v, s->iterators(1).cbdCells(dim0Cell)) {
+    dim0CellCbdSize++;
+  }
+  BOOST_CHECK_EQUAL(dim0CellCbdSize, 3);
+
+  SComplex::Iterators::DimCells::iterator::value_type dim1Cell = *(s->iterators(1).dimCells(1).begin());
+  int dim1CellCbdSize = 0;
+
+  BOOST_FOREACH(SComplex::Iterators::CbdCells::iterator::value_type v, s->iterators(1).cbdCells(dim1Cell)) {
+    dim1CellCbdSize++;
+  }
+  BOOST_CHECK_EQUAL(dim1CellCbdSize, 2);
+
+
+  SComplex::Iterators::DimCells::iterator::value_type dim2Cell = *(s->iterators(1).dimCells(2).begin());
+  int dim2CellCbdSize = 0;
+
+  BOOST_FOREACH(SComplex::Iterators::CbdCells::iterator::value_type v, s->iterators(1).cbdCells(dim2Cell)) {
+    dim2CellCbdSize++;
+  }
+  BOOST_CHECK_EQUAL(dim2CellCbdSize, 1);
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
